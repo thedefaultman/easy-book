d74660eadbb149d5c3bd26bdb4977c638856083f
Kian2002 <canadakurdistan@gmail.com>
2023-08-07 13:52:26 -0700

    create dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <Sidebar>
+      <div className="rounded-lg w-full h-full overflow-hidden overflow-y-auto">
+        main
+      </div>
+    </Sidebar>
+  );
+};
+
+export default Dashboard;
