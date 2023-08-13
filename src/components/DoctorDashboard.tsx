03bf12da7051cc6a530e94572438f936201a4a33
Kian2002 <canadakurdistan@gmail.com>
2023-08-12 22:14:51 -0700

    allow doctor to view their reviews

--- src/components/DoctorDashboard.tsx
+++ src/components/DoctorDashboard.tsx
@@ -2,9 +2,16 @@ import { useSection } from "@/hooks/useSection";
 import Appointment from "./Appointments";
 import Patients from "./Patients";
 import DoctorOverview from "./DoctorOverview";
+import { useReviewModal } from "@/hooks/useReviewModal";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useEffect } from "react";
+import ReviewModal from "./ReviewModal";
 
 const DoctorDashboard = () => {
   const { activeSection, setSection } = useSection();
+  const { onOpen, setDoctor } = useReviewModal();
+  const { session, supabaseClient: supabase } = useSessionContext();
+
   const sections = [
     {
       name: "Overview",
@@ -15,7 +22,29 @@ const DoctorDashboard = () => {
     {
       name: "Patients",
     },
+    {
+      name: "Reviews",
+    },
   ];
+
+  useEffect(() => {
+    if (session) {
+      const fetchDoctor = async () => {
+        const { data, error } = await supabase
+          .from("doctor")
+          .select("*")
+          .eq("user_id", session.user.id)
+          .single();
+        if (error) {
+          console.log(error);
+        } else {
+          setDoctor(data);
+        }
+      };
+      fetchDoctor();
+    }
+  }, [session, setDoctor, supabase]);
+
   return (
     <div className="rounded-lg w-full h-full overflow-hidden overflow-y-auto">
       <h1 className="text-3xl font-bold p-4">Doctor Dashboard</h1>
@@ -29,6 +58,10 @@ const DoctorDashboard = () => {
             }`}
             onClick={() => {
               setSection(section.name);
+              if (section.name === "Reviews") {
+                onOpen();
+                return;
+              }
             }}
           >
             {section.name}
@@ -39,6 +72,7 @@ const DoctorDashboard = () => {
       {activeSection === "Overview" && <DoctorOverview />}
       {activeSection === "Appointments" && <Appointment />}
       {activeSection === "Patients" && <Patients />}
+      {activeSection === "Reviews" && <DoctorOverview />}
     </div>
   );
 };
