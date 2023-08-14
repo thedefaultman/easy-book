6a74a838d7d0200b7a4374e5e5dd2b0c7b73e7d3
Kian2002 <canadakurdistan@gmail.com>
2023-08-14 15:52:46 -0700

    move db types to bypass linting error

--- src/providers/SupabaseProvider.tsx
+++ src/providers/SupabaseProvider.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-import { Database } from "../lib/types/types_db";
+import { Database } from "../../types_db";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 interface SupabaseProviderProps {
