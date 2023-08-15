1809eb6e574ef60e03fb564228c864053e5bdc71
Kian2002 <canadakurdistan@gmail.com>
2023-08-14 21:07:57 -0700

    add logout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { HiHome } from "react-icons/hi";
 import { CgProfile } from "react-icons/cg";
+import { IoSettingsOutline } from "react-icons/io5";
+import { useSessionContext } from "@supabase/auth-helpers-react";
 
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
@@ -15,6 +17,8 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   const { activeSection, setSection } = useSection();
+  const { session, supabaseClient: supabase } = useSessionContext();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const routes = useMemo(
     () => [
@@ -43,6 +47,41 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             {routes.map((route) => (
               <SidebarItem key={route.name} {...route} />
             ))}
+            <span
+              onClick={() => {
+                setMenuOpen(true);
+                setSection(activeSection.toString());
+              }}
+            >
+              <SidebarItem name="Settings" icon={IoSettingsOutline} />
+            </span>
+
+            {menuOpen && (
+              <div className="absolute left-0 top-0 bg-black bg-opacity-50 flex justify-center items-center w-full h-full text-center z-50">
+                <div className="bg-blue rounded-lg p-4">
+                  <h1 className="text-2xl font-bold text-center text-white">
+                    Settings
+                  </h1>
+                  <div className="flex flex-col gap-y-2 mt-4">
+                    <span
+                      className="text-white text-xl cursor-pointer"
+                      onClick={() => {
+                        setMenuOpen(false);
+                        supabase.auth.signOut();
+                      }}
+                    >
+                      Logout
+                    </span>
+                    <span
+                      className="text-white text-xl cursor-pointer"
+                      onClick={() => setMenuOpen(false)}
+                    >
+                      Close
+                    </span>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </Box>
 
